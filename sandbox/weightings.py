8b56a4660f84a2df29db294f184cd255bf82e224
uniqview <uniqview@gmail.com>
2020-05-19 10:09:44 -0700

    cleaning up data outout, need to add decibels next

--- sandbox/weightings.py
+++ sandbox/weightings.py
@@ -1,10 +1,19 @@
 #! /usr/bin/env python3
 
 
+def decibels(a: float, b: float) -> float:
+    return 20*log(a/b)
+
+
+def cents_to_ratio(x: float) -> float:
+    return 2**(x/1200)
+
+
 def log_weights_by_cents():
-    for i in range(128):
-        cents = (i * 100) / 1200
-        print("{:3.0f} {:10.4f}".format(i, cents))
+    for semitone in range(128):
+        cents = semitone * 100
+        freqs = cents_to_ratio(cents)
+        print("{:3.0f} {:6.0f} {:10.4f}".format(semitone, cents, freqs))
 
 
 def testbench():
